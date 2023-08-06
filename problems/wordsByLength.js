f54b9b8b26d8a11c30038cd9e7d7b5f4513ec3de
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-06 22:03:10 +0600

    sorted in ascending order by words length

--- problems/wordsByLength.js
+++ problems/wordsByLength.js
@@ -4,7 +4,14 @@ const sentance="Java is a programming language"
 output sortSentence="a is Java language programming"
 */
 const sentance="Java is a programming language"
-
 const sortingWordsByLength=(text)=>{
     const wordsArray=text.split(' ')
+    const sortedText=wordsArray.sort((a,b)=>{
+    //sort orginal array ke mutate kore. sort(()=>{}) method er moddhye compare function deya zay
+      return  a.length-b.length
+    })
+    return sortedText //['a', 'is', 'Java', 'language', 'programming']
 }
+
+const sortedSentence=sortingWordsByLength(sentance)
+console.log(sortedSentence);  //['a', 'is', 'Java', 'language', 'programming']
