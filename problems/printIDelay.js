1e510bf22ef29734536b11363e0e7b7a0797340c
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-06 22:55:19 +0600

    return timeout function as a promise from delay function

--- problems/printIDelay.js
+++ problems/printIDelay.js
@@ -1,10 +1,20 @@
-const timer=(n)=>{
+const delayTimeout=(i)=>{
+    return new Promise((resolve)=>{
+        setTimeout((i)=>{
+         resolve(i)
+         //promise theke zeta resolve/reject kora hoy seta onno function/variable recieve kore
+        },i*1000)
+    })
+}
+
+
+const timer=async(n)=>{
     console.log("Start timer")
     for(let i=0;i<=n;i++){
-        setTimeout(()=>{
-         console.log(i);
+        // setTimeout(()=>{
+        //  console.log(i);
          
-        },i*1000)
+        // },i*1000)
 }
 console.log("End Timer");
 
